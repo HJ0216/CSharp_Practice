2a96c1c6db8894cb0779b576e172cf2ca8032470
HJ0216 <6120hj@gmail.com>
2024-02-10 11:55:23 +0900

    Feat: CSharpPracticeApp project

---
+++ CSharpPracticeApp/Classes/BankAccount.cs
@@ -0,0 +1,82 @@
+﻿using CSharpPracticeApp.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPracticeApp.Classes
+{
+    public class BankAccount : IInformation
+    {
+        // field
+        private double balance;
+
+        // property
+        public double Balance
+        {
+            get
+            {
+                if(balance < 1000000)
+                {
+                    return balance;
+                }
+                return 1000000;
+            }
+            protected set
+            {
+                if(value > 0)
+                {
+                    balance = value;
+                } 
+                else
+                {
+                    balance = 0;
+                }
+            }
+        }
+
+        public BankAccount()
+        {
+            Balance = 100;
+        }
+        public BankAccount(double initialBalance)
+        {
+            Balance = initialBalance;
+        }
+
+        public virtual double AddToBalance(double balanceTobBeAdded)
+        {
+            Balance += balanceTobBeAdded; // Setter
+            return Balance; // Getter
+        }
+
+        public string GetInformation()
+        {
+            return $"The Balance is { Balance:c}";
+        }
+    }
+
+    public class ChildBankAccount : BankAccount
+    {
+        public ChildBankAccount()
+        {
+            Balance = 10;
+        }
+
+        public override double AddToBalance(double balanceTobBeAdded)
+        {
+            if(balanceTobBeAdded > 1000)
+            {
+                balanceTobBeAdded = 1000;
+            }
+            if (balanceTobBeAdded < -1000)
+            {
+                balanceTobBeAdded = -1000;
+            }
+
+            return base.AddToBalance(balanceTobBeAdded);
+        }
+    }
+
+}
---
+++ CSharpPracticeApp/Program.cs
@@ -0,0 +1,57 @@
+﻿using CSharpPracticeApp.Classes;
+using CSharpPracticeApp.Interfaces;
+using Microsoft.VisualBasic;
+
+namespace CSharpPracticeApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = new double[] { 1, 2, 3, 45, 67890 };
+            Console.WriteLine(SimpleMath.Add(numbers));
+            Console.WriteLine(SimpleMath.Add(10,20));
+
+
+            BankAccount bankAccount = new BankAccount(1000);
+            Console.WriteLine(bankAccount.AddToBalance(3000));
+
+            ChildBankAccount childBankAccount = new ChildBankAccount();
+            Console.WriteLine(childBankAccount.AddToBalance(3000));
+
+
+            SimpleMath simpleMath = new SimpleMath();
+            Console.WriteLine(Information(childBankAccount));
+
+        }
+
+        private static string Information(IInformation information)
+        {
+            return information.GetInformation();
+        }
+
+        class SimpleMath : IInformation
+        {
+            public static double Add(double d1, double d2)
+            {
+                return d1 + d2;
+            }
+
+            public static double Add(double[] numbers)
+            {
+                double result = 0;
+                foreach(double d in numbers)
+                {
+                    result += d;
+                }
+
+                return result;
+            }
+
+            public string GetInformation()
+            {
+                return "Class that solves simple math.";
+            }
+        }
+    }
+}

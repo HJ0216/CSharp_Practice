0b4b29ea75de485e68d18913f846322d2c0d5cf9
HJ0216 <6120hj@gmail.com>
2024-02-16 00:39:56 +0900

    Feat: Add RevitAddInPractice1 Project

---
+++ RevitAddInPractice1/Class1.cs
@@ -0,0 +1,47 @@
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAddInPractice1
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Class1 : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiApplication = commandData.Application;
+            UIDocument uiDocument = uiApplication.ActiveUIDocument;
+
+            Application application = uiApplication.Application;
+            Document document = uiDocument.Document;
+
+            /* FilteredElementCollector: search and filter the set of elements in a document
+            FilteredElementCollector collector = new FilteredElementCollector(document)
+                .OfCategory(BuiltInCategory.OST_Walls);
+            */
+
+            List<BuiltInCategory> categories = new List<BuiltInCategory>()
+            {
+                BuiltInCategory.OST_Walls,
+                BuiltInCategory.OST_Floors,
+            };
+
+            ElementMulticategoryFilter elementMulticategoryFilter = new ElementMulticategoryFilter(categories);
+
+            FilteredElementCollector collector = new FilteredElementCollector(document)
+                .WherePasses(elementMulticategoryFilter);
+
+
+            SimpleForm simpleForm = new SimpleForm(collector);
+            simpleForm.ShowDialog();
+
+            return Result.Succeeded;
+        }
+    }
+}

c593fea44ccf023724471739da449104ad48a14b
HJ0216 <6120hj@gmail.com>
2024-02-20 21:17:46 +0900

    Feat: PickACardUI Project

---
+++ PickACardUI/CardPicker.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickACardUI
+{
+    internal class CardPicker
+    {
+
+        static Random random = new Random();
+        /// <summary>
+        /// 카드를 여러 장 뽑고 반환합니다.
+        /// </summary>
+        /// <param name="numberOfCards">뽑을 카드 수</param>
+        /// <returns>카드 이름이 포함된 문자열 배열</returns>
+        public static string[] PickSomeCards(int numberOfCards)
+        {
+            string[] pickedCards = new string[numberOfCards];
+            /* foreach: 읽기 전용
+            foreach (string item in pickedCards)
+            {
+                item = RandomValue() + " of " + RandomSuit();
+            }
+            */
+
+            for (int i = 0; i < numberOfCards; i++)
+            {
+                pickedCards[i] = RandomValue() + " of " + RandomSuit();
+            }
+
+            return pickedCards;
+        }
+
+        private static string RandomSuit()
+        {
+            int value = random.Next(1, 5);
+            if (value == 1) return "Spades";
+            if (value == 2) return "Hearts";
+            if (value == 3) return "Clubs";
+            return "Diamonds";
+        }
+
+        private static string RandomValue()
+        {
+            int value = random.Next(1, 14);
+            if (value == 1) return "Ace";
+            if (value == 11) return "Jack";
+            if (value == 12) return "Queen";
+            if (value == 13) return "King";
+            return value.ToString();
+        }
+    }
+}

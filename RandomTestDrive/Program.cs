4d404e29f24bae973a364a5f0b27a8a33b64d07e
HJ0216 <6120hj@gmail.com>
2024-02-23 06:01:24 +0900

    Feat: Add RandomTestDrive Project

---
+++ RandomTestDrive/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http.Headers;
+
+namespace RandomTestDrive
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int randomInt = random.Next();
+
+            Console.WriteLine(randomInt);
+
+            int zeroToNine = random.Next(10);
+            Console.WriteLine(zeroToNine);
+
+            double randomDouble = random.NextDouble();
+            Console.WriteLine(randomDouble*100);
+            Console.WriteLine((float) randomDouble * 100F);
+            Console.WriteLine((decimal) randomDouble * 100M);
+
+            int zeroOrOne = random.Next(2);
+            bool coinFlip = Convert.ToBoolean(zeroOrOne);
+            Console.WriteLine(coinFlip);
+        }
+    }
+}

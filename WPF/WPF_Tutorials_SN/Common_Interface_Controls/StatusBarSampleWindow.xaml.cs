b37ab03848f71e0a5ce53cc5d3ea77c8b78a93ef
HJ0216 <6120hj@gmail.com>
2024-03-05 06:43:56 +0900

    Feat: Common Interface Controls and TabControls

---
+++ WPF/WPF_Tutorials_SN/Common_Interface_Controls/StatusBarSampleWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Common_Interface_Controls
+{
+    /// <summary>
+    /// Interaction logic for StatusBarSampleWindow.xaml
+    /// </summary>
+    public partial class StatusBarSampleWindow : Window
+    {
+        public StatusBarSampleWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void textEditor_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            int row = textEditor.GetLineIndexFromCharacterIndex(textEditor.CaretIndex);
+            int column = textEditor.CaretIndex - textEditor.GetLineIndexFromCharacterIndex(row);
+            cursorPosition.Text = "Line " + (row + 1) + ", Char " + (column + 1) + ", textEditor.CaretIndex" + textEditor.CaretIndex + ", " + textEditor.GetLineIndexFromCharacterIndex(row) + ", textEditor.GetLineIndexFromCharacterIndex(textEditor.CaretIndex)" + textEditor.GetLineIndexFromCharacterIndex(textEditor.CaretIndex);
+        }
+    }
+}
---
+++ WPF/WPF_Tutorials_SN/TabControls/TabControlSampleWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TabControls
+{
+    /// <summary>
+    /// Interaction logic for TabControlSampleWindow.xaml
+    /// </summary>
+    public partial class TabControlSampleWindow : Window
+    {
+        public TabControlSampleWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void previousButton_Click(object sender, RoutedEventArgs e)
+        {
+            int newIndex = tabControl.SelectedIndex - 1;
+            if(newIndex < 0)
+            {
+                newIndex = tabControl.Items.Count - 1;
+            }
+            tabControl.SelectedIndex = newIndex;
+        }
+
+        private void nextButton_Click(object sender, RoutedEventArgs e)
+        {
+            int newIndex = tabControl.SelectedIndex + 1;
+            if (newIndex >= tabControl.Items.Count)
+            {
+                newIndex = 0;
+            }
+            tabControl.SelectedIndex = newIndex;
+        }
+
+        private void selectedButton_Click(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show("Selected tab: " + (tabControl.SelectedItem as TabItem).Header); 
+            // System.Windows.Controls.StackPanel
+            MessageBox.Show("Selected tab: " + ((tabControl.SelectedItem as TabItem).Header as StackPanel).Children.OfType<TextBlock>().First().Text);
+        }
+    }
+}

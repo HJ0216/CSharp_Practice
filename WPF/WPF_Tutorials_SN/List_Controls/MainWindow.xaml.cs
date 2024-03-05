5e02d2652c17473f35564e3cb77e732820df32bd
HJ0216 <6120hj@gmail.com>
2024-03-06 07:07:29 +0900

    Feat: List Controls Project

---
+++ WPF/WPF_Tutorials_SN/List_Controls/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace List_Controls
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<ToDoItem> toDoItems = new List<ToDoItem>();
+            toDoItems.Add(new ToDoItem() { Title = "Complete this WPF tutorial", Completion = 45 });
+            toDoItems.Add(new ToDoItem() { Title = "Learn C#", Completion = 80 });
+            toDoItems.Add(new ToDoItem() { Title = "Wash the car", Completion = 0 });
+
+            toDoList.ItemsSource = toDoItems;
+        }
+    }
+
+    public class ToDoItem
+    {
+        public string Title { get; set; }
+        public int Completion { get; set; }
+    }
+}
---
+++ WPF/WPF_Tutorials_SN/List_Controls/WindowListBoxSample.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace List_Controls
+{
+    /// <summary>
+    /// Interaction logic for WindowListBoxSample.xaml
+    /// </summary>
+    public partial class WindowListBoxSample : Window
+    {
+        public WindowListBoxSample()
+        {
+            InitializeComponent();
+
+            List<TodoItem> items = new List<TodoItem>();
+            items.Add(new TodoItem() { Title = "Complete this WPF tutorial", Completion = 45 });
+            items.Add(new TodoItem() { Title = "Learn C#", Completion = 80 });
+            items.Add(new TodoItem() { Title = "Wash the car", Completion = 0 });
+
+            toDoList.ItemsSource = items;
+        }
+    }
+    public class TodoItem
+    {
+        public string Title { get; set; }
+        public int Completion { get; set; }
+    }
+}

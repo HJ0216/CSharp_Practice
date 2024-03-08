da9afea9582187d7c15bbd428c873b409c58e65c
HJ0216 <6120hj@gmail.com>
2024-03-08 23:45:18 +0900

    Feat: add ListView_Controls Project

---
+++ WPF/WPF_Tutorials_SN/ListView_Controls/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ListView_Controls
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}

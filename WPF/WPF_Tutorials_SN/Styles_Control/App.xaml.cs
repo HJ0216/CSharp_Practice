bf6226074a0b4ad8b7309b172129c8d55bb5b238
HJ0216 <6120hj@gmail.com>
2024-03-15 23:43:09 +0900

    Feat: add Styles Control Project

---
+++ WPF/WPF_Tutorials_SN/Styles_Control/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Styles_Control
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}

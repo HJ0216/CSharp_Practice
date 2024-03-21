557211c07cecd0edafcf50c115b4f211af20e1ba
HJ0216 <6120hj@gmail.com>
2024-03-22 07:33:06 +0900

    Feat: Update Smart Home App Project

---
+++ WPF/CS_WPF_UI_Academy/SmartHomeApp/SmartHomeApp/ViewModels/ViewModelCard.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace SmartHomeApp.ViewModels
+{
+    public class ViewModelCard
+    {
+		private string _title = string.Empty;
+
+		public string Title
+		{
+			get { return _title; }
+			set { _title = value; }
+		}
+
+		private bool _isChecked;
+
+		public bool IsChecked
+		{
+			get { return _isChecked; }
+			set { _isChecked = value; }
+		}
+
+		private bool _isHorizontal;
+
+		public bool IsHorizontal
+		{
+			get { return _isHorizontal; }
+			set { _isHorizontal = value; }
+		}
+
+		private ImageSource _imageOn;
+
+		public ImageSource ImageOn
+		{
+			get { return _imageOn; }
+			set { _imageOn = value; }
+		}
+
+        private ImageSource _imageOff;
+
+        public ImageSource ImageOff
+        {
+            get { return _imageOff; }
+            set { _imageOff = value; }
+        }
+
+    }
+}

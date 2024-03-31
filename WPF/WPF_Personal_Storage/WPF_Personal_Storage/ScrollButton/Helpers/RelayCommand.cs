ec1d79ee3fd643cab2a69b00439fabc5c1f8d456
HJ0216 <6120hj@gmail.com>
2024-03-31 17:00:16 +0900

    Feat: Add WPF_Personal_Storage - ScrollButton

---
+++ WPF/WPF_Personal_Storage/WPF_Personal_Storage/ScrollButton/Helpers/RelayCommand.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ScrollButton.Helpers
+{
+    public class RelayCommand : ICommand
+    {
+        Action<object> _executeMethod;
+        Func<object, bool> _canexecuteMethod;
+
+        public RelayCommand(Action<object> executeMethod)
+        {   
+            this._executeMethod = executeMethod;
+        }
+
+        public RelayCommand(Action<object> executeMethod, Func<object, bool> canexecuteMethod)
+        {
+            this._executeMethod = executeMethod;
+            this._canexecuteMethod = canexecuteMethod;
+        }
+
+        /**
+         CanExecuteChanged 이벤트에 대한 가입 및 해지를 CommandManager.RequerySuggested에 연결
+         명령의 실행 가능 상태가 변경될 때 UI가 이를 감지하고 반응할 수 있도록 함
+         예를 들어, 사용자의 액션에 따라 명령이 실행 가능하거나 불가능해질 때 UI 컨트롤이 자동으로 활성화 또는 비활성화 되도록 해줌
+         */
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            //return parameter == null ? true : _canexecuteMethod(parameter);
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            _executeMethod(parameter);
+        }
+    }
+}
---
+++ WPF/WPF_Personal_Storage/WPF_Personal_Storage/ScrollButton/ViewModels/ViewModelMain.cs
@@ -0,0 +1,133 @@
+﻿using ScrollButton.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Reflection.Metadata;
+using System.ComponentModel;
+
+namespace ScrollButton.ViewModels
+{
+    public class ViewModelMain : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+        #region Properties
+        private ImageSource _selectedImageSource;
+
+        public ImageSource SelectedImageSource
+        {
+            get { return _selectedImageSource; }
+            set 
+            {
+                _selectedImageSource = value;
+                OnPropertyChanged(nameof(SelectedImageSource));
+            }
+        }
+
+        private int _SelectedImageSourceIndex;
+
+        public int SelectedImageSourceIndex
+        {
+            get { return _SelectedImageSourceIndex; }
+            set { _SelectedImageSourceIndex = value; }
+        }
+
+        private ObservableCollection<ImageSource> _imageSources = new ObservableCollection<ImageSource>();
+
+        public ObservableCollection<ImageSource> ImageSources
+        {
+            get { return _imageSources; }
+            set { _imageSources = value; }
+        }
+
+        #endregion
+
+
+
+        #region Commands
+        public RelayCommand ScrollImageCommand => null ?? new RelayCommand(ScrollImageEvent);
+
+        #endregion
+
+
+
+        #region Constructors
+        public ViewModelMain()
+        {
+            ImageSources = new ObservableCollection<ImageSource>
+            {
+                new BitmapImage(new Uri("pack://application:,,,/Resources/number0.png"))
+                , new BitmapImage(new Uri("pack://application:,,,/Resources/number1.png"))
+                , new BitmapImage(new Uri("pack://application:,,,/Resources/number2.png"))
+                , new BitmapImage(new Uri("pack://application:,,,/Resources/number3.png"))
+                , new BitmapImage(new Uri("pack://application:,,,/Resources/number4.png"))
+                , new BitmapImage(new Uri("pack://application:,,,/Resources/number5.png"))
+                , new BitmapImage(new Uri("pack://application:,,,/Resources/number6.png"))
+                , new BitmapImage(new Uri("pack://application:,,,/Resources/number7.png"))
+                , new BitmapImage(new Uri("pack://application:,,,/Resources/number8.png"))
+                , new BitmapImage(new Uri("pack://application:,,,/Resources/number9.png"))
+            };
+
+            SelectedImageSource = ImageSources[0];
+        }
+
+        #endregion
+
+
+
+        #region Methods
+        private void ScrollImageEvent(object obj)
+        {
+            if (obj is ListView listView)
+            {
+                var scrollViewer = FindScrollViewer(listView);
+
+                if (scrollViewer != null)
+                {
+                    int middlePositionIndex = 2; // 중앙에 위치시키고자 하는 인덱스
+                    double newHorizontalOffset = SelectedImageSourceIndex - middlePositionIndex;
+
+                    scrollViewer.ScrollToHorizontalOffset(newHorizontalOffset);
+                    scrollViewer.UpdateLayout();
+                }
+            }
+        }
+
+        private ScrollViewer FindScrollViewer(DependencyObject dependencyObject)
+        {
+            if (dependencyObject is ScrollViewer)
+            {
+                return dependencyObject as ScrollViewer;
+            }
+
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(dependencyObject); i++)
+            {
+                var child = VisualTreeHelper.GetChild(dependencyObject, i);
+                var scrollViewerInListView = FindScrollViewer(child);
+                if (scrollViewerInListView != null)
+                {
+                    return scrollViewerInListView;
+                }
+            }
+
+            return null;
+        }
+
+        #endregion
+    }
+}

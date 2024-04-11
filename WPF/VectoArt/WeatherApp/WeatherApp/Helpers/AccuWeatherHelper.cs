c172c250ae20ce084343dc545e5c28d9e231142f
HJ0216 <6120hj@gmail.com>
2024-04-11 22:25:00 +0900

    Feat: add WeatherApp

---
+++ WPF/VectoArt/WeatherApp/WeatherApp/Helpers/AccuWeatherHelper.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Models;
+
+namespace WeatherApp.Helpers
+{
+    public class AccuWeatherHelper
+    {
+        public const string BASE_URL = "http://dataservice.accuweather.com/";
+        public const string AUTO_COMPLETE_ENDPOINT = "locations/v1/cities/autocomplete?apikey={0}&q={1}";
+        public const string CURRENT_CONDITIONS_ENDPOINT = "currentconditions/v1/{0}?apikey={1}";
+        public const string API_KEY = "39YxJNsXNUrkXnAFyfe7mx5jDlUdotkO";
+
+        public static async Task<List<City>> GetCities(string city)
+        // This is commonly used in asynchronous programming in C# to handle tasks that may take some time to complete,
+        // such as reading from a network stream, accessing a database, or performing I/O operations, without blocking the main thread of execution.
+        {
+            List<City> cities = new List<City>();
+            string url = BASE_URL + string.Format(AUTO_COMPLETE_ENDPOINT, API_KEY, city);
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                string json = await response.Content.ReadAsStringAsync();
+
+                cities = JsonConvert.DeserializeObject<List<City>>(json);
+            }
+
+            return cities;
+        }
+
+        public static async Task<CurrentConditions> GetCurrentConditions(string citykey)
+        {
+            CurrentConditions currentConditions = new CurrentConditions();
+
+            string url = BASE_URL + string.Format(CURRENT_CONDITIONS_ENDPOINT, citykey, API_KEY);
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                string json = await response.Content.ReadAsStringAsync();
+
+                currentConditions = JsonConvert.DeserializeObject<List<CurrentConditions>>(json).FirstOrDefault();
+            }
+
+            return currentConditions;
+        }
+    }
+}
---
+++ WPF/VectoArt/WeatherApp/WeatherApp/ViewModels/ViewModelMain.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using WeatherApp.Helpers;
+using WeatherApp.Models;
+
+namespace WeatherApp.ViewModels
+{
+    public class ViewModelMain : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+        #region Properties
+        private string _city = string.Empty;
+
+        public string City
+        {
+            get { return _city; }
+            set 
+            {
+                _city = value;
+                OnPropertyChanged("City");
+            }
+        }
+
+        public ObservableCollection<City> Cities { get; set; } = new ObservableCollection<City>();
+
+        private CurrentConditions _currentConditions = new CurrentConditions();
+
+        public CurrentConditions CurrentConditions
+        {
+            get { return _currentConditions; }
+            set
+            {
+                _currentConditions = value;
+                OnPropertyChanged("CurrentConditions");
+
+                setImageRaining(_currentConditions.HasPrecipitation);
+            }
+        }
+
+        private City _selectedCity = new City();
+
+        public City SelectedCity
+        {
+            get { return _selectedCity; }
+            set
+            {
+                _selectedCity = value;
+                OnPropertyChanged("SelectedCity");
+
+                GetCurrentConditions();
+            }
+        }
+
+        private ImageSource _imageRaining;
+
+        public ImageSource ImageRaining
+        {
+            get { return _imageRaining; }
+            set 
+            {
+                _imageRaining = value;
+                OnPropertyChanged(nameof(ImageRaining));
+            }
+        }
+
+        private string currentTime;
+        public string CurrentTime
+        {
+            get { return currentTime; }
+            set
+            {
+                if (currentTime != value)
+                {
+                    currentTime = value;
+                    OnPropertyChanged(nameof(CurrentTime));
+                }
+            }
+        }
+
+        #endregion
+
+
+
+        #region Commands
+        public ICommand SearchCommand => null ?? new RelayCommand(SearchEvent);
+        
+        #endregion
+
+
+
+        #region Constructors
+        public ViewModelMain()
+        {
+            UpdateTime();
+        }
+        #endregion
+
+
+
+        #region Methods
+        private async void GetCurrentConditions()
+        {
+            City = string.Empty;
+            //Cities.Clear();
+
+            CurrentConditions = await AccuWeatherHelper.GetCurrentConditions(SelectedCity.Key);
+        }
+
+        public async void MakeQuery()
+        {
+            var cities = await AccuWeatherHelper.GetCities(City);
+
+            Cities.Clear();
+            foreach (var city in cities)
+            {
+                Cities.Add(city);
+            }
+        }
+
+        public void SearchEvent(object obj)
+        {
+            MakeQuery();
+        }
+
+        private void setImageRaining(bool isRaining)
+        {
+            if (isRaining)
+            {
+                ImageRaining = new BitmapImage(new Uri("pack://application:,,,/Resources/loopy2_7.png"));
+            }
+            else
+            {
+                ImageRaining = new BitmapImage(new Uri("pack://application:,,,/Resources/loopy2_9.png"));
+            }
+        }
+
+        private void UpdateTime()
+        {
+            CurrentTime = DateTime.Now.ToString("HH:mm");
+
+            var timer = new System.Windows.Threading.DispatcherTimer();
+            timer.Interval = TimeSpan.FromMinutes(1);
+            timer.Tick += (sender, args) =>
+            {
+                CurrentTime = DateTime.Now.ToString("HH:mm");
+            };
+            timer.Start();
+        }
+
+        #endregion
+    }
+}

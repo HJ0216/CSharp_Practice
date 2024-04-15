f04efe17d07c99817a0ae72bcfa7243bdd9ddfbe
HJ0216 <6120hj@gmail.com>
2024-04-16 07:05:15 +0900

    Feat: update QR app

---
+++ WPF/CS_Code_Academy/QRCode/QRCode/Converters/ImageConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace QRCode.Converters
+{
+    public static class ImageConverter
+    {
+        public static BitmapImage ToBitmapImage(this Bitmap bitmap)
+        {
+            using(var memory = new MemoryStream())
+            {
+                bitmap.Save(memory, ImageFormat.Png);
+                memory.Position = 0;
+
+                var bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+                bitmapImage.Freeze();
+
+                return bitmapImage;
+            }
+        }
+    }
+}
--- WPF/CS_Code_Academy/QRCode/QRCode/ViewModels/ViewModelMain.cs
+++ WPF/CS_Code_Academy/QRCode/QRCode/ViewModels/ViewModelMain.cs
@@ -1,5 +1,7 @@
 ﻿using MessagingToolkit.QRCode.Codec;
 using MessagingToolkit.QRCode.Codec.Data;
+using Microsoft.Win32;
+using QRCode.Converters;
 using QRCode.Helpers;
 using QRCode.Models;
 using QRCode.Views.UserControls;
@@ -8,12 +10,15 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace QRCode.ViewModels
 {
@@ -65,6 +70,44 @@ namespace QRCode.ViewModels
             set { _textQr = value; }
         }
 
+        private ImageSource _imageQr;
+
+        public ImageSource ImageQr
+        {
+            get { return _imageQr; }
+            set 
+            {
+                _imageQr = value;
+                OnPropertyChange(nameof(ImageQr));
+            }
+        }
+
+        public Bitmap bitmap;
+
+
+        private ImageSource _imageScanQr;
+
+        public ImageSource ImageScanQr
+        {
+            get { return _imageScanQr; }
+            set
+            {
+                _imageScanQr = value;
+                OnPropertyChange(nameof(ImageScanQr));
+            }
+        }
+
+        private string _textScanQr = string.Empty;
+
+        public string TextScanQr
+        {
+            get { return _textScanQr; }
+            set 
+            {
+                _textScanQr = value;
+                OnPropertyChange(nameof(TextScanQr));
+            }
+        }
 
         #endregion
 
@@ -73,8 +116,12 @@ namespace QRCode.ViewModels
         #region Commands
         public ICommand WindowDragCommand => null ?? new RelayCommand(WindowDragEvent);
         public ICommand ApplicationShutDownCommand => null ?? new RelayCommand(ApplicationShutDownEvent);
+        public ICommand ChangeTabCommand => null ?? new RelayCommand(ChangeTabEvent);
         public ICommand CreateQRCommand => null ?? new RelayCommand(CreateQREvent);
         public ICommand SaveQRCommand => null ?? new RelayCommand(SaveQREvent);
+        public ICommand ScanQRCommand => null ?? new RelayCommand(ScanQREvent);
+
+        
 
         #endregion
 
@@ -86,8 +133,7 @@ namespace QRCode.ViewModels
             MenuModels = new ObservableCollection<MenuModel>()
             {
                 new MenuModel("Create QR", "Qrcode")
-                , new MenuModel("Edit QR", "QrcodeEdit")
-                , new MenuModel("Access Point", "AccessPointSuccess")
+                , new MenuModel("Scan QR", "QrcodeEdit")
             };
 
             CurrentView = new UserControlCreateQR();
@@ -112,19 +158,67 @@ namespace QRCode.ViewModels
             Application.Current.Shutdown();
         }
 
+        private void ChangeTabEvent(object param)
+        {
+            if(param is MenuModel)
+            {
+                string title = (param as MenuModel).Title;
+
+                switch (title)
+                {
+                    case "Create QR":
+                        CurrentView = new UserControlCreateQR();
+                        CurrentView.DataContext = this;
+                        break;
+                    case "Scan QR":
+                        CurrentView = new UserControlScanQR();
+                        CurrentView.DataContext = this;
+                        break;
+                    default:
+                        CurrentView = new UserControlCreateQR();
+                        CurrentView.DataContext = this;
+                        break;
+                }
+            }
+            Console.WriteLine(param.ToString());
+        }
+
+
         private void CreateQREvent(object obj)
         {
             QRCodeEncoder qRCodeEncoder = new QRCodeEncoder();
             qRCodeEncoder.QRCodeScale = 8;
 
-            Bitmap bitmap = qRCodeEncoder.Encode(TextQr);
+            bitmap = qRCodeEncoder.Encode(TextQr);
+            ImageQr = Converters.ImageConverter.ToBitmapImage(bitmap);
         }
 
         private void SaveQREvent(object obj)
         {
-            throw new NotImplementedException();
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "PNG|*.png";
+            saveFileDialog.FileName = TextQr;
+
+            if(saveFileDialog.ShowDialog() == true)
+            {
+                if(bitmap != null)
+                {
+                    bitmap.Save(saveFileDialog.FileName, ImageFormat.Png);
+                }
+            }
         }
 
+        private void ScanQREvent(object obj)
+        {
+            QRCodeDecoder qRCodeDecoder = new QRCodeDecoder();
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            if(openFileDialog.ShowDialog() == true)
+            {
+                ImageScanQr = new BitmapImage(new Uri(openFileDialog.FileName));
+                TextScanQr = qRCodeDecoder.Decode(new QRCodeBitmapImage(new Bitmap(openFileDialog.FileName)));
+            }
+        }
 
 
         #endregion

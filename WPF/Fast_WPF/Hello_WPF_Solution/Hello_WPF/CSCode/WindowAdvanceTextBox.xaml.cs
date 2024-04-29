8f5350edfd35d1c4f598b2bbd4c822f6a862e59e
HJ0216 <6120hj@gmail.com>
2024-04-30 06:47:26 +0900

    Feat: update Hello WPF Solution

---
+++ WPF/Fast_WPF/Hello_WPF_Solution/Hello_WPF/CSCode/WindowAdvanceTextBox.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Hello_WPF.CSCode
+{
+    /// <summary>
+    /// Interaction logic for WindowAdvanceTextBox.xaml
+    /// </summary>
+    public partial class WindowAdvanceTextBox : Window
+    {
+        public WindowAdvanceTextBox()
+        {
+            InitializeComponent();
+        }
+
+        private void zoomIn_Click(object sender, RoutedEventArgs e)
+        {
+            int maxFontSize = 72;
+            
+            if(mainTextBox.FontSize >= maxFontSize)
+            {
+                mainTextBox.FontSize = maxFontSize;
+                return;
+            }
+
+            mainTextBox.FontSize += 2;
+        }
+
+        private void zoomOut_Click(object sender, RoutedEventArgs e)
+        {
+            int minFontSize = 8;
+
+            if (mainTextBox.FontSize <= minFontSize)
+            {
+                mainTextBox.FontSize = minFontSize;
+                return;
+            }
+
+            mainTextBox.FontSize -= 2;
+        }
+
+        private void selectAll_Click(object sender, RoutedEventArgs e)
+        {
+            mainTextBox.Focus();
+            mainTextBox.SelectAll();
+        }
+
+        private void undo_Click(object sender, RoutedEventArgs e)
+        {
+            mainTextBox.Undo();
+            mainTextBox.UndoLimit = 5;
+        }
+
+        private void redo_Click(object sender, RoutedEventArgs e)
+        {
+            mainTextBox.Redo();
+        }
+
+        private void copy_Click(object sender, RoutedEventArgs e)
+        {
+            mainTextBox.Copy();
+        }
+
+        private void cut_Click(object sender, RoutedEventArgs e)
+        {
+            mainTextBox.Cut();
+        }
+
+        private void paste_Click(object sender, RoutedEventArgs e)
+        {
+            mainTextBox.Paste();
+        }
+
+        bool isWrap = false;
+
+        private void isWrapButton_Click(object sender, RoutedEventArgs e)
+        {
+            isWrap = !isWrap;
+            if(isWrap == true)
+            {
+                isWrapButton.Content = "Disable Wrap";
+                mainTextBox.TextWrapping = TextWrapping.Wrap;
+                mainTextBox.HorizontalScrollBarVisibility = ScrollBarVisibility.Hidden;
+                return;
+            }
+            if(isWrap == false)
+            {
+                isWrapButton.Content = "Enable Wrap";
+                mainTextBox.TextWrapping = TextWrapping.NoWrap;
+                mainTextBox.HorizontalScrollBarVisibility = ScrollBarVisibility.Auto;
+                return;
+            }
+        }
+    }
+}
---
+++ WPF/Fast_WPF/Hello_WPF_Solution/Hello_WPF/CSCode/WindowTextInfo.xaml.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Hello_WPF.CSCode
+{
+    /// <summary>
+    /// Interaction logic for WindowTextInfo.xaml
+    /// </summary>
+    public partial class WindowTextInfo : Window
+    {
+        public WindowTextInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(inputId.Text == "")
+            {
+                MessageBox.Show("The ID is empty!", "Alert");
+                return;
+            }
+            if (inputFirstName.Text == "")
+            {
+                MessageBox.Show("The First Name is empty!", "Alert");
+                return;
+            }
+            if (inputLastName.Text == "")
+            {
+                MessageBox.Show("The Last Name is empty!", "Alert");
+                return;
+            }
+
+            string fileName = string.Empty;
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.InitialDirectory = @"C:";
+            saveFileDialog.DefaultExt = "txt";
+
+            saveFileDialog.Filter = "Normal Text File|*.txt";
+
+            if(saveFileDialog.ShowDialog() == true)
+            {
+                fileName = saveFileDialog.FileName.ToString();
+
+                string contents = "ID: " + inputId.Text + "\r\n"
+                    + "First Name: " + inputFirstName.Text + "\r\n"
+                    + "Last Name: " + inputLastName.Text;
+
+                File.WriteAllText(fileName, contents, Encoding.UTF8);
+
+                // Path to Debug that execution file located.
+                //string applicationPath = AppDomain.CurrentDomain.BaseDirectory;
+
+                inputId.Text = string.Empty;
+                inputFirstName.Text = string.Empty;
+                inputLastName.Text = string.Empty;
+
+                inputId.Focus();
+            }
+
+        }
+    }
+}

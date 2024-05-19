61078eb2e4ec6181cfd51e1499b75c0e2a1f31cb
HJ0216 <6120hj@gmail.com>
2024-05-19 09:24:52 +0900

    Feat: update All Selector

---
+++ WPF/Fast_WPF/Hello_WPF_Solution/Hello_WPF/CSCode/WindowWPFEvent.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Hello_WPF.CSCode
+{
+    /// <summary>
+    /// Interaction logic for WindowWPFEvent.xaml
+    /// </summary>
+    public partial class WindowWPFEvent : Window
+    {
+        public WindowWPFEvent()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_KeyDown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show(e.SystemKey.ToString() + " / " + e.Key.ToString());
+        }
+    }
+}

7b9878c0c5bdf222077b32c49d7ea0e964a2cb1f
HJ0216 <6120hj@gmail.com>
2024-05-21 22:15:47 +0900

    Feat: update All Selector

--- WPF/WPF_Personal_Storage/WPF_Personal_Storage/AllSelector/ViewModels/ViewModelMain.cs
+++ WPF/WPF_Personal_Storage/WPF_Personal_Storage/AllSelector/ViewModels/ViewModelMain.cs
@@ -10,104 +10,200 @@ namespace AllSelector.ViewModels
 {
     class ViewModelMain : INotifyPropertyChanged
     {
-        #region INotifyPropertyChanged
-        public event PropertyChangedEventHandler? PropertyChanged;
+        private bool _isAllChecked;
+        private bool _isOneChecked;
+        private bool _isTwoChecked;
+        private bool _isThreeChecked;
+        private bool _isFourChecked;
+        private bool _ignoreCheckAll;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public bool IsAllChecked
+        {
+            get => _isAllChecked;
+            set
+            {
+                if (_isAllChecked != value)
+                {
+                    _isAllChecked = value;
+                    OnPropertyChanged(nameof(IsAllChecked));
+                    if (!_ignoreCheckAll)
+                    {
+                        _ignoreCheckAll = true;
+                        IsOneChecked = value;
+                        IsTwoChecked = value;
+                        IsThreeChecked = value;
+                        IsFourChecked = value;
+                        _ignoreCheckAll = false;
+                    }
+                }
+            }
+        }
+
+        public bool IsOneChecked
+        {
+            get => _isOneChecked;
+            set
+            {
+                if (_isOneChecked != value)
+                {
+                    _isOneChecked = value;
+                    OnPropertyChanged(nameof(IsOneChecked));
+                    UpdateIsAllChecked();
+                }
+            }
+        }
+
+        public bool IsTwoChecked
+        {
+            get => _isTwoChecked;
+            set
+            {
+                if (_isTwoChecked != value)
+                {
+                    _isTwoChecked = value;
+                    OnPropertyChanged(nameof(IsTwoChecked));
+                    UpdateIsAllChecked();
+                }
+            }
+        }
+
+        public bool IsThreeChecked
+        {
+            get => _isThreeChecked;
+            set
+            {
+                if (_isThreeChecked != value)
+                {
+                    _isThreeChecked = value;
+                    OnPropertyChanged(nameof(IsThreeChecked));
+                    UpdateIsAllChecked();
+                }
+            }
+        }
+
+        public bool IsFourChecked
+        {
+            get => _isFourChecked;
+            set
+            {
+                if (_isFourChecked != value)
+                {
+                    _isFourChecked = value;
+                    OnPropertyChanged(nameof(IsFourChecked));
+                    UpdateIsAllChecked();
+                }
+            }
+        }
+
+        private void UpdateIsAllChecked()
+        {
+            if (!_ignoreCheckAll)
+            {
+                _ignoreCheckAll = true;
+                IsAllChecked = _isOneChecked && _isTwoChecked && _isThreeChecked && _isFourChecked;
+                _ignoreCheckAll = false;
+            }
+        }
 
         protected virtual void OnPropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+    }
+    /*    {
+            #region INotifyPropertyChanged
+            public event PropertyChangedEventHandler? PropertyChanged;
 
-        #endregion
-
-
-
-        #region Properties
-        private bool _isNeedAllCheckBoxUpdate;
-
-        private bool _isAllChecked;
-
-        public bool IsAllChecked
-        {
-            get { return _isAllChecked; }
-            set 
+            protected virtual void OnPropertyChanged(string propertyName)
             {
-                _isAllChecked = value;
-                OnPropertyChanged(nameof(IsAllChecked));
-
-                ChangeAllCheckboxStatus();
-
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
             }
-        }
 
-        private bool _isOneChecked;
+            #endregion
 
-        public bool IsOneChecked
-        {
-            get { return _isOneChecked; }
-            set
+
+
+            #region Properties
+            private bool _isAllChecked;
+
+            public bool IsAllChecked
             {
-                _isOneChecked = value;
-                OnPropertyChanged(nameof(IsOneChecked));
-
-                ChangeCheckboxStatus();
-            }
-        }
-
-        private bool _isTwoChecked;
-
-        public bool IsTwoChecked
-        {
-            get { return _isTwoChecked; }
-            set
-            {
-                _isTwoChecked = value;
-                OnPropertyChanged(nameof(IsTwoChecked));
-
-                ChangeCheckboxStatus();
-            }
-        }
-
-        #endregion
-
-
-
-        #region Methods
-        private void ChangeAllCheckboxStatus()
-        {
-            if (_isNeedAllCheckBoxUpdate)
-            {
-                return;
-            }
-            else
-            {
-                if (IsAllChecked)
+                get { return _isAllChecked; }
+                set 
                 {
-                    IsOneChecked = true;
-                    IsTwoChecked = true;
+                    _isAllChecked = value;
+                    OnPropertyChanged(nameof(IsAllChecked));
+
+                    ChangeCheckboxFromAll();
+
+                }
+            }
+
+            private bool _isOneChecked;
+
+            public bool IsOneChecked
+            {
+                get { return _isOneChecked; }
+                set
+                {
+                    _isOneChecked = value;
+                    OnPropertyChanged(nameof(IsOneChecked));
+
+                    ChangeCheckboxFromEach();
+                }
+            }
+
+            private bool _isTwoChecked;
+
+            public bool IsTwoChecked
+            {
+                get { return _isTwoChecked; }
+                set
+                {
+                    _isTwoChecked = value;
+                    OnPropertyChanged(nameof(IsTwoChecked));
+
+                    ChangeCheckboxFromEach();
+                }
+            }
+
+            #endregion
+
+
+
+            #region Methods
+            private void ChangeCheckboxFromAll()
+            {
+                bool alreadyCheckedAll = IsOneChecked && IsTwoChecked;
+
+                if (IsAllChecked == alreadyCheckedAll)
+                {
+                    return;
                 }
                 else
                 {
-                    IsOneChecked = false;
-                    IsTwoChecked = false;
+                    IsOneChecked = IsAllChecked;
+                    IsTwoChecked = IsAllChecked;
                 }
+
             }
-        }
 
-        private void ChangeCheckboxStatus()
-        {
-            bool allChecked = IsOneChecked && IsTwoChecked;
-
-            if (IsAllChecked != allChecked)
+            private void ChangeCheckboxFromEach()
             {
-                _isNeedAllCheckBoxUpdate = true;
+                bool alreadyCheckedAll = IsOneChecked && IsTwoChecked;
+
+                if (IsAllChecked == alreadyCheckedAll)
+                {
+                    return;
+                }
+                else
+                {
+                    IsAllChecked = alreadyCheckedAll;
+                }
+
             }
-
-            IsAllChecked = allChecked;
-
-            _isNeedAllCheckBoxUpdate = false;
-        }
-
-        #endregion
-        }
-    }
+            #endregion
+        }*/
+}

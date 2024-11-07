24218d47b2fe30f5cc08136706cf4d0673ee532e
HJ0216 <6120hj@gmail.com>
2024-11-08 05:09:18 +0900

    Feat: add FlipCardApp

---
+++ WPF/FlipCardApp/FlipCardApp/LayoutListbox.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace FlipCardApp
+{
+    internal class LayoutListbox : ListBox
+    {
+        public Layout Layout
+        {
+            get { return (Layout)GetValue(LayoutProperty); }
+            set { SetValue(LayoutProperty, value); }
+        }
+
+        public static readonly DependencyProperty LayoutProperty =
+            DependencyProperty.Register("Layout", typeof(Layout), typeof(LayoutListbox), new PropertyMetadata(Layout.List));
+        // PropertyMetadata: LayoutProperty의 기본값을 Layout.List로 지정
+        /**
+         public static readonly DependencyProperty LayoutProperty = 
+            DependencyProperty.Register(
+                "Layout",                      // 속성 이름
+                typeof(Layout),                // 속성 유형
+                typeof(LayoutListbox),         // 소유자 유형
+                new PropertyMetadata(
+                    Layout.List,               // 기본값
+                    OnLayoutChanged));         // 변경 알림 콜백, 속성 값이 변경될 때 UI를 업데이트하거나 추가 작업을 수행
+         */
+    }
+
+    public enum Layout
+    {
+        Tile,
+        List
+    }
+}
---
+++ WPF/FlipCardApp/FlipCardApp/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FlipCardApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            initData();
+        }
+
+        private void initData()
+        {
+            var data = new[]
+            {
+                new {Name = "James"}
+                , new {Name = "Mike"}
+                , new {Name = "John"}
+                , new {Name = "Luke"}
+                , new {Name = "Oscar"}
+                , new {Name = "Charles"}
+            };
+
+            myListbox.ItemsSource = data;
+        }
+
+        private void tileMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            myListbox.Layout = Layout.Tile;
+        }
+
+        private void listMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            myListbox.Layout = Layout.List;
+        }
+    }
+}
